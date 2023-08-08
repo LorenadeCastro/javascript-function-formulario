e87b17ced3f82595471a2586c938e3079f2f7cb1
Lorena de Castro <139800465+LorenadeCastro@users.noreply.github.com>
2023-08-08 16:26:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const clube = {time: "Clube Atletico Mineiro",
+cidade: "Belo Horizonte",
+ano: "1908",
+cor: "preto e branco",
+mascote: "Galo Doido",
+torcida: "Maior de Minas",
+grito: function(){alert('Galooooooooo')},
+cantico: function(){alert('Forte e Vingador')},
+hino: function(){alert('Vencer, Vencer, Vencer, esse é o nosso ideal. Honramos o nome de Minas.')}
+};
+
+console.log (clube.grito())
+console.log (clube.cantico())
+console.log (clube.hino())
